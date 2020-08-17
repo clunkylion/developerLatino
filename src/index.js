5bb1e320c0b6e1e6ed750ca963039a04e13bd58b
Jose <cote99salamanca@gmail.com>
2020-08-17 19:16:01 -0400

    eslint config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
-ReactDOM.render(
-    <HelloWorld />, document.getElementById('app')
-)
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./container/App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
