db89ede572ee12e1992bf217863f78522dffa21d
Jose <cote99salamanca@gmail.com>
2020-08-19 16:43:53 -0400

    json-server

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/styles/components/Carousel.scss";
+
+export const Carousel = ({ children }) => {
+  return (
+    <div>
+      <section className="carousel">
+        <div className="carousel__container">{children}</div>
+      </section>
+    </div>
+  );
+};
+export default Carousel;
