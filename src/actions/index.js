c08923e201b2af79948af45b018d3f90237126a0
José luis Cortés Molina <josecortes@Joses-MacBook-Pro.local>
2020-08-23 01:53:12 -0400

    se agregaro las acciones de redux

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const setFavorite = (payload) => ({
+  type: "SET_FAVORITE",
+  payload,
+});
+export const deleteFavorite = (payload) => ({
+  type: "DELETE_FAVORITE",
+  payload,
+});
+export const loginRequest = (payload) => ({
+  type: "LOGIN_REQUEST",
+  payload,
+});
