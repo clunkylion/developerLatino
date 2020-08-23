da4903f7b0b550545af9a3e4b24d4e11b2117a15
José luis Cortés Molina <josecortes@Joses-MacBook-Pro.local>
2020-08-23 03:11:44 -0400

    add Redux in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import proptypes from "prop-types";
 import gravatar from "../utils/gravatar";
+import { logoutRequest } from "../actions/index";
 import "../assets/styles/components/Header.scss";
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
@@ -9,6 +11,9 @@ import userIcon from "../assets/static/user-icon.png";
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
 
   return (
     <header className="header">
@@ -25,21 +30,37 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href="/">Cuenta</a>
-          </li>
-          <li>
-            <Link to="/login">Iniciar Sesión</Link>
-          </li>
+          {hasUser ? (
+            <li>
+              <a href="/">{user.name}</a>
+            </li>
+          ) : null}
+          {hasUser ? (
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Cerrar Sesion
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Iniciar Sesión</Link>
+            </li>
+          )}
         </ul>
       </div>
     </header>
   );
 };
-
+Header.proptypes = {
+  user: proptypes.object.isRequired,
+  logoutRequest: proptypes.func.isRequired,
+};
 const mapStateToProps = (state) => {
   return {
     user: state.user,
   };
 };
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
